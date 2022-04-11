2d7652006ae66b7327a6ced9e04da316f133d4d3
Sarushan Thiyakumar <sarushan.thiy@gmail.com>
2022-04-11 13:09:28 +0100

    added docker-compse files, updated models, routes and database

--- api/dbconfig/setup.sql
+++ api/dbconfig/setup.sql
@@ -9,15 +9,42 @@ CREATE TABLE users (
 DROP TABLE IF EXISTS habits;
 
 CREATE TABLE habits (
-    id serial PRIMARY KEY
-
+    id serial PRIMARY KEY,
+    name VARCHAR (255),
+    -- measurement VARCHAR (255),
+    frequency VARCHAR (255),
+    currStreak INT DEFAULT 0,
+    streakEnd VARCHAR (255) NOT NULL,
+    user_id INT NOT NULL,
+    currentTime timestamp DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL;
 );
+
+CREATE TABLE habit_counter (
+    id serial PRIMARY KEY,
+    habit_id INT REFERENCES habits(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    time_done timestamp DEFAULT CURRENT_TIMESTAMP,
+    completed BOOLEAN
+)
 
 INSERT INTO users (username, password_digest)
 VALUES 
-( 'Rey', '' ),
-( 'Mark', '');
+( 'Rey', 'tu9ibtoi4tbh2hhuet' ),
+( 'Mark', 'h4hoeuba3r3tbaeu');
 
-INSERT INTO habits ()
+INSERT INTO habits (user_id, habit_id, name, frequency)
 VALUES
-();
+(1, 1, Drink Water, '3 day'),
+(1, 2, Workout, '1 day');
+
+
+INSERT INTO habit_counter (habit_id, time_done, completed)
+VALUES
+(1, CURRENT_TIMESTAMP - INTERVAL '1 day', FALSE),
+(1, CURRENT_TIMESTAMP - INTERVAL '1 day', FALSE),
+(1, CURRENT_TIMESTAMP - INTERVAL '1 day', TRUE)
+
+(1, CURRENT_TIMESTAMP - INTERVAL '2 day', FALSE),
+(1, CURRENT_TIMESTAMP - INTERVAL '2 day', FALSE),
+(1, CURRENT_TIMESTAMP - INTERVAL '2 day', FALSE),
+
